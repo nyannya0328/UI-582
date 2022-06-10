ba1b855edd299f43c4eea8137058236988a7107f
nyannya0328 <thecuba54@gmail.com>
2022-06-10 15:42:13 +0900

    Initial Commit

---
+++ UI-582/UI_582App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_582App.swift
+//  UI-582
+//
+//  Created by nyannyan0328 on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct UI_582App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
